170e74fc0863a0eb31120ba2320a4751e7cd2ed0
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2022-11-16 21:29:24 +0800

    Initial Commit
    Added some components and containers for the website

---
+++ firstreactwebsite/src/components/brand/Brand.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./brand.css";
+
+const Brand = () => {
+  return (
+    <div>
+      <h1>My website</h1>
+    </div>
+  );
+};
+
+export default Brand;
---
+++ firstreactwebsite/src/containers/whatWebsite/Whatwebsite.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatwebsite.css";
+
+const Whatwebsite = () => {
+  return <div>Article</div>;
+};
+
+export default Whatwebsite;
